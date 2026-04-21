baa10c017ae9843c8ea29c2f150b162ca1943696
Aknur Sharipzhan <sharipzhan.aknur@mail.ru>
2026-04-21 18:27:15 +0200

    settings fix

--- stripe_payments/stripe_payments/settings.py
+++ stripe_payments/stripe_payments/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key')
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = [h.strip() for h in os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
